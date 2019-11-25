4c3762572821381afbfcaebe5184c36a66f48e2a
Tyler Corwin <Tyler.C@figmints.com>
2019-11-25 13:05:19 -0500

    successful login and creation of JWT

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -2,8 +2,8 @@ class V1::SessionsController < ApplicationController
   def create
     user = User.where(email: params[:email]).first
 
-    if user.valid_password?(params[:password])
-      render json: user.as_json(only: [:id, :email]), status: :created
+    if user&.valid_password?(params[:password])
+      render json: user.as_json(only: [:email, :authentication_token]), status: :created
     else
       head(:unauthorized)
     end
