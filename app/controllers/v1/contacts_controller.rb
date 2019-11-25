4a1ae8e9b0d7f25877db0d67545dd9189f562236
Tyler Corwin <Tyler.C@figmints.com>
2019-11-25 09:18:46 -0500

    test commite

---
+++ app/controllers/v1/contacts_controller.rb
@@ -0,0 +1,30 @@
+class V1::ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+
+    render json: @contacts, status: :ok
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    @contact.save
+    render json: @contact, status: :created
+  end
+
+  def destroy
+    @contact = Contact.where(id: params[:id]).first
+    if @contact.destroy
+      head(:ok)
+    else
+      # Rails way of just returning the content in the header so we dont loose any information 
+      head(:unprocessable_entity)
+    end
+
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :email)
+    end
+end
